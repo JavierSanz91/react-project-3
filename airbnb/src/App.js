e0bf1c553068c0a4434df7f639eddf7f0d306c19
[Javier <[sanzalvarezjavier@gmail.com]>
2023-07-04 18:10:05 +0200

    added content to card component

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,15 +2,43 @@ import React from "react"
 import Navbar from "./modules/Navbar"
 import Hero from "./modules/Hero"
 import Card from "./modules/Card"
+import swimmer from "./images/swimmer.png"
+import wedding from "./images/wedding.png"
+import bike from "./images/bike.png"
+
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
-            <Card />
-            <Card />
+            <Card
+                src= {swimmer}
+                status="SOLD OUT"
+                score="5.0"
+                reviews="(6) · "
+                location="USA"
+                name="Life lessons with Katie Zaferes"
+                price="From $136 "
+            />
+            <Card 
+                src= {wedding}
+                status="ONLINE"
+                score="5.0"
+                reviews="(30) · "
+                location="USA"
+                name="Learn wedding photography"
+                price="From $125 "
+            />
+            <Card 
+                src= {bike}
+                status="ONLINE"
+                score="4.8"
+                reviews="(2) · "
+                location="USA"
+                name="Group Mountain Biking"
+                price="From $50 "            
+            />
         </div>
     )
 }
--- airbnb/src/modules/Card.js
+++ airbnb/src/modules/Card.js
@@ -1,25 +1,24 @@
 import React from "react"
-import swimmer from "../images/swimmer.png"
 import star from "../images/star.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div>
             <div className="card-photo-status">
-                <img className="card-photo" src={swimmer} alt="swimmer" />
-                <p className="card-status">SOLD OUT</p>
+                <img className="card-photo" src={props.src} alt="swimmer" />
+                <p className="card-status">{props.status}</p>
             </div>
             <div className="reviews-block">
                 <img className="star-icon" src={star} alt="star" />
-                <p className="card-score">5.0</p>
-                <p className="card-reviews" >(6) ·</p>
-                <p className="card-location">USA</p>
+                <p className="card-score">{props.score}</p>
+                <p className="card-reviews" >{props.reviews}</p>
+                <p className="card-location">{props.location}</p>
             </div>
             <div>
-                <p className="card-name">Life lessons with Katie Zaferes</p>
+                <p className="card-name">{props.name}</p>
             </div>
             <div>
-                <p className="card-price"><span className="card-price-bold">From $136 </span>/ person</p>
+                <p className="card-price"><span className="card-price-bold">{props.price} </span>/ person</p>
             </div>
         </div>
     )
