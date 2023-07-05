4d31c84408a1e7edd8a0f389c6eea32e74b9658c
[Javier <[sanzalvarezjavier@gmail.com]>
2023-07-05 19:00:20 +0200

    simplified mapping

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -10,14 +10,7 @@ export default function App() {
     const experiencesElements = experiencesData.map(experience => {
         return  <Card
             key={experience.id}
-            image= {experience.image}
-            status= {experience.status}
-            score= {experience.score}
-            reviews= {experience.reviews}
-            location=  {experience.location}
-            name=  {experience.name}
-            price=  {experience.price}
-            openSpot= {experience.openSpot}   // Add this line
+            experience={experience}
         />
     })
     
--- airbnb/src/modules/Card.js
+++ airbnb/src/modules/Card.js
@@ -5,20 +5,20 @@ export default function Card(props) {
     return (
         <div className="card">
             <div className="card-photo-status">
-                <img className="card-photo" src={props.image} alt="swimmer" />
-                {props.openSpot === 0 && <p className="card-status">{props.status}</p>}
+                <img className="card-photo" src={props.experience.image} alt="swimmer" />
+                {props.experience.openSpot === 0 && <p className="card-status">{props.experience.status}</p>}
             </div>
             <div className="reviews-block">
                 <img className="star-icon" src={star} alt="star" />
-                <p className="card-score">{props.score}</p>
-                <p className="card-reviews" >{props.reviews}</p>
-                <p className="card-location">{props.location}</p>
+                <p className="card-score">{props.experience.score}</p>
+                <p className="card-reviews" >{props.experience.reviews}</p>
+                <p className="card-location">{props.experience.location}</p>
             </div>
             <div>
-                <p className="card-name">{props.name}</p>
+                <p className="card-name">{props.experience.name}</p>
             </div>
             <div>
-                <p className="card-price"><span className="card-price-bold">{props.price} </span>/ person</p>
+                <p className="card-price"><span className="card-price-bold">{props.experience.price} </span>/ person</p>
             </div>
         </div>
     )
